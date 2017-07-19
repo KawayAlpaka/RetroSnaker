544a2eb2e24297a65172a0e27e3cdcfe72500531
L <my917@qq.com>
2017-07-19 16:43:24 +0800

    update

--- src/pages/test-list/test-list.ts
+++ src/pages/test-list/test-list.ts
@@ -16,17 +16,20 @@ export class TestListPage {
 
   items:any;
   constructor(public navCtrl: NavController, public navParams: NavParams,private myEle:ElementRef) {
-    this.refresh();
   }
   refresh(){
     this.items = [];
     for(let i=0;i<15;i++){
       this.items.push(i);
     }
-  }
+    setTimeout(()=>{
+      this.myEle.nativeElement.querySelector(".scroll-content").scrollTop = 90;
+    },100);
 
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad TestListPage');
+    this.refresh();
   }
 
   doInfinite(infiniteScroll) {
@@ -45,7 +48,6 @@ export class TestListPage {
     setTimeout(() => {
       this.refresh();
       console.log('t Async operation has ended');
-      this.myEle.nativeElement.querySelector(".scroll-content").scrollTop = 90;
       infiniteScroll.complete();
     }, 3000);
   }
